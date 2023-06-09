86c8f858e122892a300a50f3acd6da11b650ac6b
Kausik S S <kausikselvaraj14@gmail.com>
2023-06-09 23:44:54 +0530

    Create folders for file types

--- organiser.js
+++ organiser.js
@@ -1,12 +1,28 @@
 const pathName = require('./index.js');
 let fs = require('fs');
 
+fs.mkdir(pathName + '/word Files', (error) => {
+    if(error){
+        console.log('Word Files folder could not be created');
+    }
+})
+fs.mkdir(pathName + '/Excel Files', (error) => {
+    if(error){
+        console.log('Excel files folder could not be created');
+    }
+})
+fs.mkdir(pathName + '/Text Files', (error) => {
+    if(error){
+        console.log('Text files folder could not be created');
+    }
+})
+
 fs.readdir(pathName, (err, files) => {
     if (err) {
       console.error(err);
       return;
     }
-    console.log(files);    
+        
  }
 );
 
